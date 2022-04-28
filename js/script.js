8511212c587ce28d3b5a57eb44f433055cfb093f
clemchret <clement.chret@gmail.com>
2022-04-28 02:03:25 +0200

    Fonction d'ajout des tâches

--- js/script.js
+++ js/script.js
@@ -14,7 +14,8 @@ document.querySelector('.color-theme').addEventListener('click', setTheme);
 
 
 //Apparition ou non du bouton reset liste et de la barre des filtres
-function loadFilters (){
+
+function loadFilters(){
     const resetBtn = document.querySelector('.resetBtn');
     const filterBar = document.querySelector('.stat');
     const listItem = document.querySelector('.list-items');
@@ -41,50 +42,63 @@ document.getElementById('resetList').addEventListener('click', resetTask);
 window.onload = loadTasks;
 
 //On prend les tâches dans le local storage et on les convertit en tableau.
-function loadTasks(e){
-    e.preventDefault();
-    let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
-    if(typeof tasks != null){
-    //On passe dans tous les éléments créés et on les affiche.
-    tasks.forEach(task => {
-        const list = document.querySelector('ul');
-        const li = document.createElement('li');
-        li.classList.add('item');
-        li.innerHTML = 
-        `<div class="task-bar">
-            <!--<i class="fa-regular fa-circle-check"></i>-->
-            <i class="fa-regular fa-circle"></i>
-            <input class="task-bar-title" type="text" value="${task.task}" disabled>
-            <i class="fa-solid fa-pen-to-square"></i>
-            <i class="far fa-trash-alt"></i>
-        </div>`;
-        list.insertBefore(li, list.children[0]);
-    });
+function loadTasks(){
+    let tasks = localStorage.getItem("tasks");
+    if(!tasks){
+        tasks = null;
+    }else{
+            //On passe dans tous les éléments créés et on les affiche.
+            tasks = Array.from(JSON.parse(tasks));
+            tasks.forEach(task => {
+                const list = document.querySelector('ul');
+                const li = document.createElement('li');
+                li.classList.add('item');
+                li.innerHTML = 
+                `<div class="task-bar">
+                    <!--<i class="fa-regular fa-circle-check"></i>-->
+                    <i class="fa-regular fa-circle"></i>
+                    <input class="task-bar-title" type="text" value="${task.task}" disabled>
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="far fa-trash-alt"></i>
+                </div>`;
+                list.insertBefore(li, list.children[0]);
+            });
     };
-
+    //On vérifie si il faut afficher les filtres après le chargement d'éventuelles tâches.
+    loadFilters();
 };
 
 
-//Fonction d'ajout des tâches
+//Fonction d'ajout des tâches.
 function addTask(){
     
     let inputTask = document.getElementById('toDo').value;
 
-    /*//Si rien n'est entré.
+    //Si rien n'est entré.
     if(inputTask === ''){
         alert('Veuillez entrer une tâche !');
-        return false;
-    }
+        return;
+    }else{
         //Si c'est entré, vérifier si elle n'existe pas déjà.
-        let savedTasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
-        //Si elle existe déjà.
-        savedTasks.forEach(todo => {
-            if (todo.task === inputTask) {
-                alert("Tâche déjà créée");
-                inputTask.value = "";
-                return;
-            }
-        });*/
+        let savedTasks = localStorage.getItem("tasks");
+
+        //Si il n'y a pas de tâche, passer directement à l'ajout.
+        if(savedTasks === null){
+            savedTasks = '';
+        }else{
+            //Si il y a des tâches, vérification de son éventuelle redondance.
+            savedTasks = Array.from(JSON.parse(savedTasks));
+            savedTasks.forEach(todo => {
+                if (todo.task === inputTask) {
+                    alert("Tâche déjà créée");
+                    document.getElementById('toDo').value = '';
+                    return;
+                }
+            });
+
+            //Return sur le else pour terminer la fonction et ne pas passer par l'ajout de la tâche.
+            return;
+        }
 
         //Ajout de la tâche dans le local storage.
         localStorage.setItem("tasks", JSON.stringify([...JSON.parse(localStorage.getItem("tasks") || "[]"), { task: document.getElementById('toDo').value, completed: false }]));
@@ -104,7 +118,11 @@ function addTask(){
 
         //clear le champ input pour pouvoir entrer la prochaine valeur.
         document.getElementById('toDo').value = '';
-    }
+
+        //On ajoute les filtres après ajout de la tâche
+        loadFilters();
+    };
+};
 
 document.getElementById('submitTask').addEventListener('click', addTask);
 
